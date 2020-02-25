3f3376c65f357d982972747c2efe073a0c6b9ad1
Jonas Vogel <jonas@vogel.xyz>
2020-02-25 15:40:51 +0100

    Implement download function, moving export to xlsx to optinal argument Add function for output youtube-dl commands Add function for output video links Add fix for filenaming if name include slash

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
-Get DMAX Links
+dmax-scraper
 ==============
+
+Based on https://github.com/Brawl345/Get-DMAX-Links. But with direct donwload function.
+
 This handy python script gets links of a DMAX show. You can also specify a season with `-s` and an episode with `-e`.
 
-The resulting information will be saved in an Excel file.
+There are 4 options:
+- It will download automatic found files and saves filenames into files for tracking already downloaded files. \[DEFAULT\]
+- The resulting information will be saved in an Excel file: `--xls`
+- Print links directly: `--links`
+- Print youtube-dl commands directly: `--commands`
 
 ## Usage
+The Script is written in Python 3.
+
 1. Clone repo
 2. `pip install -U -r requirements.txt`
-3. `python dmax.py NAME-OF-SHOW [-s SEASON] [-e EPISODE] [--specials]`
+3. `python dmax.py [-i NAME-OF-SHOW] [-s SEASON] [-e EPISODE] [--specials] [--xlsx] [--links] [--commands]`
 4. Check help with `python dmax.py -h`
 
 ## How it works
 1. Contacts DMAX API and gets video ids plus sonicToken from cookie
 2. Sends sonicToken and video id(s) to the player API which returns the link(s)
-
-![Screenshot](https://raw.githubusercontent.com/Brawl345/Get-DMAX-Links/master/screenshot.png)
+3. Download files and saves filename into `downloaded.txt`
--- dmax.py
+++ dmax.py
@@ -2,20 +2,34 @@
 import argparse
 import logging
 import os
-
+import sys
+import youtube_dl
 import xlsxwriter
 from requests import get
 
 import formats
 
-logger = logging.getLogger("DMAX")
 logging.getLogger('requests.packages.urllib3.connectionpool').setLevel('WARNING')
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(levelname)-7s] (%(asctime)s) %(filename)s::%(lineno)d %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='main.log')
+logger = logging.getLogger("DMAX")
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('[%(levelname)-7s] (%(asctime)s) %(filename)s::%(lineno)d %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 
 BASE_URL = "https://www.dmax.de/"
 API_URL = BASE_URL + "api/show-detail/{0}"
+API_URL_ALL_SHOWS = BASE_URL + "api/shows-az/"
 PLAYER_URL = "https://sonic-eu1-prod.disco-api.com/playback/videoPlaybackInfo/"
 USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
+MAX_DOWNLOAD = 500
+ALREADY_DOWNLOADED_FILE = "downloaded.txt"
 
 
 class WorkbookWriter:
@@ -55,16 +69,7 @@ class WorkbookWriter:
     def __del__(self):
         self.workbook.close()
 
-
-def main(showid, chosen_season=0, chosen_episode=0, includespecials=False):
-    if chosen_episode < 0 or chosen_season < 0:
-        print("ERROR: Episode/Season must be > 0.")
-        return
-    if chosen_episode > 0 and chosen_season == 0:
-        print("ERROR: Season must be set.")
-        return
-
-    logger.info("Getting show data")
+def get_episodes(showid, chosen_season=0, chosen_episode=0, includespecials=True):
     try:
         req = get(API_URL.format(showid))
     except Exception as e:
@@ -118,15 +123,10 @@ def main(showid, chosen_season=0, chosen_episode=0, includespecials=False):
         logger.info("No Episodes to download.")
         return
 
-    xlsname = "{0}.xlsx".format(showid)
-    file_num = 0
-    while os.path.isfile(xlsname):
-        file_num += 1
-        xlsname = "{0}-{1}.xlsx".format(showid, file_num)
-    xls = WorkbookWriter(xlsname)
+    return_dict = []
+    logger.info("Get {} links".format(len(episodes)))
 
     for num, episode in enumerate(episodes):
-        logger.info("Getting link {0} of {1}".format(num + 1, len(episodes)))
         if episode.season == "" and episode.episode == "":
             filename = "{show_name} - {episode_name}".format(
                 show_name=show.show.name,
@@ -146,9 +146,6 @@ def main(showid, chosen_season=0, chosen_episode=0, includespecials=False):
                 episode=episode.episode,
                 episode_name=episode.name
             )
-        xls.worksheet.write(xls.row, xls.col(), episode.name)
-        xls.worksheet.write(xls.row, xls.col(), episode.description)
-        xls.worksheet.write(xls.row, xls.col(), filename)
 
         try:
             req = get(PLAYER_URL + episode.id, headers={
@@ -157,32 +154,80 @@ def main(showid, chosen_season=0, chosen_episode=0, includespecials=False):
             })
         except Exception as exception:
             logger.error("Connection for video id {0} failed: {1}".format(episode.id, str(exception)))
-            xls.row += 1
-            xls._col = 0
             continue
 
         if req.status_code != 200:
             logger.error("HTTP error code {0} for video id {1]".format(req.status_code, episode.id))
-            xls.row += 1
-            xls._col = 0
             continue
 
         data = req.json()
         video_link = data["data"]["attributes"]["streaming"]["hls"]["url"]
-        xls.worksheet.write(xls.row, xls.col(), video_link)
+        filename = filename.replace("/", "-")
+
+        return_dict.append({'name': episode.name, 'description': episode.description, 'filename': filename, 'video_link': video_link, 'dir': "{}/{} Staffel {}".format(show.show.name.replace("/", "-"), show.show.name.replace("/", "-"), episode.season)})
+    return return_dict
+
+
+def request_dmax_api_all_shows():
+    response = get(API_URL_ALL_SHOWS)
+    return response.json()
+
+
+def extract_alternate_id(data):
+    return_list = []
+    for i in data["items"]:
+        ##this is sort by A, B, C...
+        for j in i["items"]:
+            return_list.append(j["url"][11:])
+    return return_list
+
+
+def already_downloaded(episode_filename):
+    found = False
+    with open(ALREADY_DOWNLOADED_FILE, "r") as f:
+        line = f.readline()
+        while line:
+            if episode_filename == line.strip():
+                found = True
+                break
+            line = f.readline()
+    return found
+
+
+def set_downloaded(episode_filename):
+    with open(ALREADY_DOWNLOADED_FILE, "a") as f:
+        f.write("{}\n".format(episode_filename))
+
+
+def write_to_xls(show, episodes):
+    xlsname = "{}.xlsx".format(show)
+    file_num = 0
+    while os.path.isfile(xlsname):
+        file_num += 1
+        xlsname = "{0}-{1}.xlsx".format(showid, file_num)
+    xls = WorkbookWriter(xlsname)
+
+    for episode in episodes:
+        xls.worksheet.write(xls.row, xls.col(), episode['name'])
+        xls.worksheet.write(xls.row, xls.col(), episode['description'])
+        xls.worksheet.write(xls.row, xls.col(), episode['filename'])
+
+        xls.worksheet.write(xls.row, xls.col(), episode['video_link'])
         xls.worksheet.write(xls.row, xls.col(start=True),
-                            "youtube-dl \"{0}\" -o \"{1}.mp4\"".format(video_link, filename)
+                            "youtube-dl \"{0}\" -o \"{1}.mp4\"".format(episode['video_link'], episode['filename'])
                             )
 
         xls.row += 1
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Gets direct links for DMAX series")
+    parser = argparse.ArgumentParser(description="Gets direct links for DMAX series and download them or save to xlsx file")
     parser.add_argument(
-        "alternateId",
+        "-i",
+        metavar="Show ID",
+        dest="id",
         type=str,
-        help="alternateId of the series (last part of URL)"
+        help="alternateId of the series (last part of URL), e.g. american-chopper"
     )
     parser.add_argument(
         "-s",
@@ -207,10 +252,90 @@ if __name__ == "__main__":
         dest='includespecials',
         help='Download specials'
     )
-    arguments = parser.parse_args()
-    main(
-        showid=arguments.alternateId,
-        chosen_season=arguments.season,
-        chosen_episode=arguments.episode,
-        includespecials=arguments.includespecials
+    parser.add_argument(
+        '--xls',
+        action='store_true',
+        default=False,
+        dest='xls',
+        help='Instead of downloading files, save links into xslx file'
     )
+    parser.add_argument(
+        '--links',
+        action='store_true',
+        default=False,
+        dest='links',
+        help='Output links directly'
+    )
+    parser.add_argument(
+        '--commands',
+        action='store_true',
+        default=False,
+        dest='commands',
+        help='Output youtube download commands directly'
+    )
+    arguments = parser.parse_args()
+
+    showid = arguments.id,
+    showid = showid[0]
+    chosen_season = arguments.season,
+    chosen_season = chosen_season[0]
+    chosen_episode = arguments.episode,
+    chosen_episode = chosen_episode[0]
+    includespecials = arguments.includespecials
+    xlsx = arguments.xls
+    out_links = arguments.links
+    out_commands = arguments.commands
+    downloaded_count = 0
+
+    if showid is None:
+        json_data = request_dmax_api_all_shows()
+        alternate_id_list = extract_alternate_id(json_data)
+        logger.info("Found following shows: {}".format(alternate_id_list))
+    else:
+        if chosen_episode < 0 or chosen_season < 0:
+            print("ERROR: Episode/Season must be > 0.")
+
+        if chosen_episode > 0 and chosen_season == 0:
+            print("ERROR: Season must be set.")
+
+        alternate_id_list = [showid]
+
+
+    for show in alternate_id_list:
+        logger.info("Processing Show: {}".format(show))
+        episodes = get_episodes(show, chosen_season=chosen_season, chosen_episode=chosen_episode, includespecials=includespecials)
+
+        if episodes is None:
+            logger.warning("No Episodes in {}".format(show))
+            continue
+
+        if xlsx:
+            write_to_xls(show, episodes)
+        elif out_links:
+            for episode in episodes:
+                print(episode['video_link'])
+        elif out_commands:
+            for episode in episodes:
+                print("youtube-dl \"{0}\" -o \"{1}.mp4\"".format(episode['video_link'], episode['filename']))
+        else:
+            if not os.path.isfile(ALREADY_DOWNLOADED_FILE):
+                open(ALREADY_DOWNLOADED_FILE, 'a').close()
+
+            for j in episodes:
+                if downloaded_count == MAX_DOWNLOAD:
+                    logger.info("Max download count reached. Stopping")
+                    sys.exit(0)
+
+                if not already_downloaded(j.get("filename")):
+                    logger.info("Downloading file: {}".format(j.get("filename")))
+                    ydl_opts = {'quiet': True, 'outtmpl': "downloads/{}/{}".format(j.get("dir"), j.get("filename"))}
+                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                        ydl.download([j.get("video_link")])
+
+                    if os.path.isfile("downloads/{}/{}.mp4".format(j.get("dir"), j.get("filename"))):
+                        logger.info("Downloading file finished: {}".format(j.get("filename")))
+                        set_downloaded(j.get("filename"))
+                    else:
+                        logger.warning("Downloading file failed: {}".format(j.get("filename")))
+
+                downloaded_count += 1
